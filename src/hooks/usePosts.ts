9a0e90bbc276fe0dfca5785881df33572ff872ff
Chuong <hoangchuongitutc@gmail.com>
2024-04-02 02:51:51 +0700

    feat(dev-env): show notification via socket when have new shared video

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -4,26 +4,45 @@ import { config } from '@/constants/config';
 import { NEW_SHARED_VIDEO } from '@/constants/socket';
 import { fetchPosts } from '@/services/posts.service';
 import { Post } from '@/types/posts.type';
+import { Bounce, toast } from 'react-toastify';
+
+const socket = io(config.apiBaseUrl as string);
 
 const usePosts = () => {
   const [posts, setPosts] = useState<Post[]>([]);
+
+  const getAllPosts = async () => {
+    const response = await fetchPosts({ page: 1, perPage: 10 });
+    setPosts(response);
+  };
+
   //   responseable to fetch intital data through api.
   useEffect(() => {
-    const getAllPosts = async () => {
-      const response = await fetchPosts({ page: 1, perPage: 10 });
-      console.log('Re-fresh Post list');
-      setPosts(response);
-    };
-
     getAllPosts();
   }, []);
 
   //   subscribes to realtime updates when post is added on server.
   useEffect(() => {
-    const socket = io(config.apiBaseUrl as string);
+    const handleMessage = ({ title, user }: Post) => {
+      const msg = `${user.fullName} just shared a new video: ${title}`;
+
+      toast.info(msg, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+        transition: Bounce,
+      });
+
+      getAllPosts();
+    };
+
     socket.on(NEW_SHARED_VIDEO, (newData: Post) => {
-      console.log('New video added');
-      setPosts((prevData) => [...prevData, newData]);
+      handleMessage(newData);
     });
   }, []);
 
